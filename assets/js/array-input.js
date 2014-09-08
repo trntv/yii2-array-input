293dbcb22dc38502feef18738a0c179f835189e2
Eugine Terentev <eugine@terentev.net>
2014-09-08 13:02:07 +0300

    prevent default

--- assets/js/array-input.js
+++ assets/js/array-input.js
@@ -1,10 +1,12 @@
 (function( jQuery ) {
     jQuery(document).ready(function(){
-        jQuery(document).on('click', '.array-input-remove', function(){
+        jQuery(document).on('click', '.array-input-remove', function(e){
+            e.preventDefault();
             jQuery(this).parents('.input-group').remove();
             return false;
         });
-        jQuery(document).on('click', '.array-input-plus', function(){
+        jQuery(document).on('click', '.array-input-plus', function(e){
+            e.preventDefault();
             var inputGroup = jQuery(this).parents('.input-group');
             var inputClone = inputGroup.clone();
             inputClone.find('input').val('');
@@ -13,7 +15,7 @@
                 .addClass('array-input-remove')
             .find('i')
                 .removeClass('glyphicon-plus')
-                .addClass('glyphicon-remove')
+                .addClass('glyphicon-remove');
             return false;
         });
     })
